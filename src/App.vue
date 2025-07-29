296b60a3197eaab32bc57c935f386b7bc9f5d652
Ali Ozcan <a.ozcan98@hotmail.com>
2025-07-29 15:09:41 +0200

    refactor: change starter code

--- src/App.vue
+++ src/App.vue
@@ -38,22 +38,11 @@ const books = [
         <div class="book-section">
           <h3 class="section-title">A list of my books ({{ books.length }})</h3>
           <div class="book-items">
-            <!-- Todo: Loop through books and show the books one by one. -->
-            <div class="book-item">
-              <div class="book-content">
-                <h3 :class="['book-title', { read: books[0].isRead }]">
-                  {{ books[0].title }}
-                </h3>
-                <p :class="['book-author', { read: books[0].isRead }]">by {{ books[0].author }}</p>
-              </div>
-              <div class="book-actions">
-                <button :class="['read-button', { read: books[0].isRead }]">
-                  {{ books[0].isRead ? 'Read' : 'Mark as Read' }}
-                </button>
-                <button class="delete-button">delete</button>
-              </div>
-            </div>
-            <!-- End book item -->
+            <!-- Todo: Add a list of books -->
+            <ul>
+              <li class="book-title">{{ books[0].title }} - {{ books[0].author }}</li>
+              <li class="book-title">{{ books[1].title }} - {{ books[1].author }}</li>
+            </ul>
           </div>
         </div>
       </div>
