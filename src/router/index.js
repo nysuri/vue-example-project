e19f958d44f5a726652f179bcb8f01196f360f5b
Ali <a.ozcan98@hotmail.com>
2025-10-24 11:18:09 +0200

    feat: add not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PartiesView from '@/views/PartiesView.vue'
 import PartyDetailView from '@/views/PartyDetailView.vue'
+import PathNotFoundView from '@/views/PathNotFoundView.vue'
 
 const routes = [
   { path: '/', redirect: '/parties' },
   { path: '/parties', component: PartiesView },
   { path: '/party/:id', component: PartyDetailView },
+  { path: '/:pathMatch(.*)*', component: PathNotFoundView },
   // Todo: Add ForumView here mapped to /forum
 ]
 
---
+++ src/views/PathNotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not-found">
+    <h2>404 - Page Not Found</h2>
+    <p>Sorry, the page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.not-found {
+  text-align: center;
+  margin-top: 100px;
+}
+</style>
